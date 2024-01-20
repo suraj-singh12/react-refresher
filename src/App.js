a2d485b69ae8ffe4e0229b8ee3843fdf4662ce45
suraj-singh12 <surajsingh5092@gmail.com>
2024-01-20 20:57:07 +0530

    counter using useReducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+};
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case ACTIONS.INCREMENT: 
+       return ({ count: state.count + 1 });  
+    case ACTIONS.DECREMENT: 
+      return ({ count: state.count -1});
+    default: 
+      return state;
+  }
 }
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count : 0}); 
+
+  return (
+    <div>
+      <button onClick={() => dispatch({type: ACTIONS.INCREMENT})}> + </button>    
+      <span> {state.count} </span>
+      <button onClick={() => dispatch({ type: ACTIONS.DECREMENT })}> - </button>
+    </div>
+  )
+};
 
 export default App;
